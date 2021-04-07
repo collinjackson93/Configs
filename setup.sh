d9254095a8e35d125fb721aa71c072ac03c2b820
Collin Jackson <cjackson@belvederetrading.com>
2021-04-06 21:16:47 -0500

    Initial setup
    Added .vimrc, .bashrc (called .commonrc), and .spacemacs.d, along with a bash
    script to easily get set up on a new host

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+readonly thisDir=$(dirname $0)
+
+createLink() {
+    ln -insr $thisDir/$1 ~/"$1"
+}
+
+createLink .vimrc
+createLink .spacemacs.d
+
+# Check if the .bashrc already sources .commonrc
+if ! grep -q -e 'source.*\.commonrc' ~/.bashrc; then
+   echo "source $thisDir/.commonrc" >> ~/.bashrc
+fi
